01932b0be694ba1daf3c78619b58b1cfd4745676
Vitória Ferreira <vitoria.m.oliveira@ufv.br>
2023-10-02 17:22:30 -0300

    testes

--- meu_app_testes/test/meu_app_testes_test.dart
+++ meu_app_testes/test/meu_app_testes_test.dart
@@ -1,8 +1,16 @@
-import 'package:meu_app_testes/meu_app_testes.dart';
+import 'package:meu_app_testes/meu_app_testes.dart' as app;
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('Calcula o desconto do produto sem porcentagem', () {
+    expect(
+      app.calcularDesconto((1000), 150, false), 850
+    );
+  });
+
+  test('Calcula o desconto do produto com porcentagem', () {
+    expect(
+      app.calcularDesconto((1000), 15, true), 850
+    );
   });
 }
