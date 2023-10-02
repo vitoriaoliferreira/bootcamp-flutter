51f323e27851a8b51a4e852a894fe5e3953267e3
Vitória Ferreira <vitoria.m.oliveira@ufv.br>
2023-10-02 15:55:22 -0300

    tipos de dados e variaveis

--- meu_app/bin/meu_app.dart
+++ meu_app/bin/meu_app.dart
@@ -1,5 +1,17 @@
 import 'package:meu_app/meu_app.dart' as meu_app;
 
 void main(List<String> arguments) {
-  print('Hello world: ${meu_app.calculate()}!');
+  String texto = "vitoria";
+  var texto2 = "vitoria";
+  String texto3;
+
+  int numero1 = 1;
+  var numero2=2;
+  int numero3;
+
+  List<String> lista = [];
+  lista.add("vitoria");
+  var lista1 = [];
+  lista1.add(02);
+  lista1.add("vitoria");
 }
